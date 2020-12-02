fb62ad92085ae206c32eb65d64756fa6cafd5548
surinder singh <surinder83singh@gmail.com>
2020-12-03 04:00:22 +0530

    init

---
+++ http/app.js
@@ -0,0 +1,127 @@
+import '/style/style.js';
+import {dpc, camelCase, html, UID, FlowApp} from '/flow/flow-ux/flow-ux.js';
+export *  from './faucet-form.js';
+
+class App extends FlowApp {
+	constructor(){
+		super();
+		window.app = this;
+		//this.initSiteConfig(config)
+		this.opt = {
+
+		}
+
+		this.initLog();
+		dpc(async ()=>this.init());
+		this.registerListener("popstate", (e)=>{
+			let {menu="home", args=[]} = e.state||{};
+			console.log(`popstate: ${document.location}, state: ${JSON.stringify(e.state)}`)
+			this.setMenu(menu, args, true);
+		});
+	}
+
+	async init(){
+		await this.initSocketIORPC({
+			args:{
+				transports:["websocket"]
+			}
+		});
+		//await this.initSocketIONATS();
+		await this.initUI();
+		dpc(()=>this.setLoading(false));
+	}
+
+	async initUI(){
+		this.bodyEl = document.body;
+		//this.initTabs();
+	}
+	initTabs(){
+		/*
+		this.menuEl = document.querySelector("#left-menu");
+		this.menuEl.addEventListener("click", e=>{
+			let el = e.target.closest("[data-menu]");
+			if(!el 
+				|| el.classList.contains("active")
+				|| el.classList.contains("disabled"))
+				return
+
+			this.setMenu(el.getAttribute("data-menu"));
+		});
+
+		this.initialUrlState = this.getStateFromUrl();
+		*/
+	}
+
+	handleWorkspaceMenu(menu, el){
+		let fn = camelCase(`on-${menu}-click`);
+		if(this[fn])
+			this[fn](el);
+	}
+
+	getStateFromUrl(){
+		//TODO 
+		//let args, menu = window.location.hash.replace("#", "");
+		let paths = window.location.pathname.replace(/^\/{1,}|\/{1,}$/g, "").split("/");
+		///[menu, ...args] = menu.split(":")
+		let menu = paths.shift() || "home";
+		let args = paths;
+		console.log("getStateFromUrl", menu, args)
+		return {menu, args, paths};
+	}
+	updateUrlState(data, title, url){
+		if(!url)
+			url = `/${data.menu}/${data.args.join("/")}`;
+		if(title){
+			title = camelCase(data.menu);
+			title = title[0].toUpperCase()+title.substring(1);
+		}
+
+		history.replaceState(data, title, url);
+	}
+
+	setMenu(menu, args=[], skipHistory=false){
+		this.fire("before-menu-change", {menu, args, app:this}, {}, window);
+		this.log("menu", menu)
+		this.activateMenu(menu, args, skipHistory)
+	}
+	activateMenu(menu, args=[], skipHistory=false){
+		//window.location.hash = `${menu}${args.length?':'+args.join(":"):''}`;
+		if(!skipHistory){
+			let url = `/${menu}${args.length?'/'+args.join("/"):''}`;
+			history.pushState({menu, args}, menu, url);
+		}
+
+		document.querySelectorAll(".left-area .menu li, .tab-content, [for][slot='header']").forEach(el=>{
+			let m = el.getAttribute("data-menu") || el.getAttribute("for");
+			if(el.classList.contains("active")){
+				if(m == menu)
+					return
+				el.classList.remove("active")
+			}
+			if(m == menu){
+				el.classList.add("active")
+				el.handleUrlArgs?.(args);
+			}
+		})
+	}
+
+	initUrlState(){
+		dpc(200, ()=>{
+			let {menu, args} = this.initialUrlState || this.getStateFromUrl();
+			let component = document.querySelector(`.menu-handler[for='${menu}'],.tab-content[for='${menu}']`);
+			//console.log("component", component.constructor)
+			if(!component || !component.handleUrl){
+				this.setMenu(menu, args||[]);
+			}else{
+				component.handleUrl(menu, args||[], this);
+			}
+		})
+	}
+
+	signoutCallback(){
+		super.signoutCallback?.();
+		this.setMenu("home")
+	}
+}
+
+App.define("kaspa-app");
---
+++ http/faucet-form.js
@@ -0,0 +1,80 @@
+import {dpc, html, css, BaseElement} from '/flow/flow-ux/flow-ux.js';
+
+export class FaucetForm extends BaseElement {
+	static get properties(){
+		return {
+			errorMessage:{type:String}
+		}
+	}
+	static get styles(){
+		return css`
+			:host{display:block;max-width:500px;margin:auto}
+			flow-select{margin:8px 0px;}
+			.error{color:red;min-height:30px;padding:5px;box-sizing:border-box;}
+		`
+	}
+
+	render(){
+		return html`
+			<flow-input label="Address" class="address"
+				placeholder="kaspatest:qq0nvlmn07f6edcdfynt4nu4l4r58rkquuvgt635ac">
+			</flow-input>
+			<flow-select label="Network" selected="mainnet" class="network">
+				<flow-menu-item value="testnet">TESTNET</flow-menu-item>
+				<flow-menu-item value="mainnet">MAINNET</flow-menu-item>
+			</flow-select>
+			<flow-input label="Amount" class="amount"
+				placeholder="10">
+			</flow-input>
+			<div class="error">${this.errorMessage}</div>
+			<flow-btn primary @click="${this.submit}">SUBMIT</flow-btn>
+		`;
+	}
+
+	constructor(){
+		super();
+	}
+
+	submit(){
+		let qS = this.renderRoot.querySelector.bind(this.renderRoot);
+		let address = qS(".address").value;
+		let network = qS(".network").value;
+		let amount = qS(".amount").value;
+
+		console.log("address, network, amount:::", address, network, amount)
+
+		if(!address)
+			return this.setError("Please enter address");
+
+		amount = parseFloat(amount||"", 10);
+		if(isNaN(amount) || !amount || amount<1 || amount>1000)
+			return this.setError("Please enter amount between 1-1000");
+
+		this.setError(false);
+
+		app.rpc.dispatch("faucet-request", {
+			address, network, amount
+		},(err, result)=>{
+			if(err){
+				this.setError(err);
+				return
+			}
+
+			this.setError(false);
+			FlowDialog.alert("Success", `We have sent ${result.amount} to ${address}.`);
+		})
+	}
+
+	setError(err){
+		if(!err){
+			this.errorMessage = "";
+			return
+		}
+
+		this.errorMessage = err.error||err;
+
+	}
+
+}
+
+FaucetForm.define("faucet-form");
