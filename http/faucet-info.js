7523645ae97c0991f1a18fe6eec1246b3e29be78
supertypo <suprtypo@pm.me>
2025-04-25 01:25:00 +0200

    No mining / no overfill info

--- http/faucet-info.js
+++ http/faucet-info.js
@@ -42,8 +42,12 @@ export class FaucetInfo extends BaseElement {
                 <div class="caption">Welcome to Kaspa Faucet</div>
                 <div class="info-content">
                     <p>Kaspa Faucet sends Kaspa to anyone requesting.</p>
-                    <p>It has a defined address, where you can send or mine Kaspa.
-                    If the faucet address has enough Kaspa, it will send it to an address you provide. </p>
+                    <p>It has a defined address, where you can send Kaspa.
+                        If the faucet address has enough Kaspa, it will send it to an address you provide. </p>
+                    <p style="color: #FFCCCB"><b>Please DO NOT mine to the Faucet address directly.</b><br/>
+                        Mine to your own wallet, then send larger amounts.</p>
+                    <p style="color: #FFCCCB"><b>Maximum supported balance is ~90M TKAS.</b><br/>
+                        Do not overfill it.</p>
                     <p>Faucet can receive funds at the following address:</p>
                     <p><b class="address-box">${this.address}</b></p>
                     <p>Requests are limited to the maximum of <b>${KAS(this.limit)} KAS</b> per IP address, per <b>24 hours</b>.</p>
